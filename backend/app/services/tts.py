6839c26a45ac67484d6635a3dd49358e1a4e5361
lucasnye <141851379+lucasnye@users.noreply.github.com>
2025-06-17 00:30:49 +0800

    text-to-speech push

--- backend/app/services/tts.py
+++ backend/app/services/tts.py
@@ -1,1 +1,22 @@
 from gtts import gTTS
+import pygame
+from tempfile import NamedTemporaryFile
+
+def read_text():
+    with open("../../../sampleData/sample_text_for_tts.txt", "r") as text:
+        content = text.read()
+        return content
+
+def text_to_speech(text):
+    with NamedTemporaryFile(delete=False, suffix=".mp3") as temp_audio:
+        audio = gTTS(text=text, lang='en', slow=True)
+        audio.save(temp_audio.name)
+    
+    pygame.mixer.init()
+    pygame.mixer.music.load(temp_audio.name)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
+
+text = read_text()
+text_to_speech(text)
